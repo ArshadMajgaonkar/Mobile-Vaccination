438cafa694b4e320a6425cd49ec37f0f6d6c66cd
hkaranjule77 <hkaranjule77@gmail.com>
2022-01-16 22:17:48 +0530

    Initial Commit

---
+++ src/main/java/com/aghs/mobilevaccination/MobileVaccinationApplication.java
@@ -0,0 +1,13 @@
+package com.aghs.mobilevaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileVaccinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileVaccinationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aghs/mobilevaccination/MobileVaccinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aghs.mobilevaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileVaccinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
