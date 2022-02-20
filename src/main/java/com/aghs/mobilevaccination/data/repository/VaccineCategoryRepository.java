40d635383ec5f4fbd09bb7c9595ed3f591ed4904
hkaranjule77 <hkaranjule77@gmail.com>
2022-02-20 16:29:37 +0530

    bug fix

--- src/main/java/com/aghs/mobilevaccination/data/repository/VaccineCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aghs.mobilevaccination.data.repository;
-
-import com.aghs.mobilevaccination.data.model.VaccineCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VaccineCategoryRepository extends JpaRepository<VaccineCategory, String> {
-}
